07853df2abe557b42ca0d492bc5fbd2cf242ef8b
Nikita Bilous <nikita@bilous.me>
2019-12-04 14:30:09 +0300

    delete

--- test/test_mysql.py
+++ test/test_mysql.py
@@ -1,7 +1,8 @@
 
-# from noorm.adapter.mysql import AdapterMySQL as m
+import pytest
 import noorm
 import os
+import MySQLdb
 
 MYSQL_DSN = os.environ.get('NOORM_TEST_MYSQL_DSN')
 
@@ -21,13 +22,47 @@ def test_mysql():
         create table test_noorm (
             id serial,
             dt datetime not null default now(),
-            name varchar(32),
+            name varchar(32) UNIQUE,
             price float,
             unique (name)
         )
     """, commit=False)
     db.execute_sql('insert into test_noorm (name, price) values (%s, %s)', ['test', 12.34])
     row = db.fetchrow('select id, name, price from test_noorm', where={'id': 1})
-    assert dict(row) == {'id': 1, 'name': 'test', 'price': 12.34}
+    assert row == {'id': 1, 'name': 'test', 'price': 12.34}
     rows = db.fetch('select id, name, price from test_noorm', where={'id': 1})
     assert rows == [{'id': 1, 'name': 'test', 'price': 12.34}]
+
+    # def fetchcol(self, *args, **kwargs):
+    col = db.fetchcol('select id from test_noorm')
+    assert col == [1]
+
+    # def fetchval(self, *args, **kwargs):
+    tid = db.fetchval('select id from test_noorm')
+    assert tid == 1
+
+    # def fetchdict(self, *args, **kwargs):
+    rows = db.fetchdict('select id, name, price from test_noorm')
+    assert rows == {1: {'id': 1, 'name': 'test', 'price': 12.34}}
+
+    # def fetchkv(self, *args, **kwargs):
+    rows = db.fetchkv('select id, price from test_noorm')
+    assert rows == {1: 12.34}
+
+    tid = db.insert('test_noorm', {'name': 'foo', 'price': 22})
+    assert tid == 2
+    
+    tid = db.insert('test_noorm', [{'name': 'foo1', 'price': 22}, {'name': 'foo2', 'price': 23}])
+    assert db.fetchval('select count(*) from test_noorm') == 4
+
+    with pytest.raises(MySQLdb._exceptions.IntegrityError):
+        tid = db.insert('test_noorm', {'name': 'foo', 'price': 22})
+
+    tid = db.insert('test_noorm', {'name': 'foo', 'price': 33}, update='name')
+    assert db.fetchval('select price from test_noorm', where={'name': 'foo'}) == 33
+
+
+    # print(db.fetchcol('select price from test_noorm order by 1'))
+    res = db.delete('test_noorm', where={'price': {'gt': 22}})
+    assert res == 2
+
