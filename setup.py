194b2a4bdfcaf50975f4db06db16a5b87d846c20
Nikita Bilous <nikita@bilous.me>
2019-12-06 15:23:31 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='noorm',
+    version='0.0.1',
+    packages=find_packages(),
+    long_description="Noorm - write queries pythonic-way with the power of raw sql",
+)
